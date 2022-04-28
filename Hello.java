a990acfcf87ecc75b81ecc1014158598805a39c3
高大泽 <gdz060010@163.com>
2022-04-28 16:02:09 +0800

    master修改

--- Hello.java
+++ Hello.java
@@ -5,6 +5,7 @@ public static void main (String args[]){
 	System.out.println("Hello world!!!");
 	System.out.println("Hello world!!!");
 	System.out.println("ÇÐ»»µ½DeV!!!");
+	System.out.println("master£¡£¡£¡");
 
 }
 {
